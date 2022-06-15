90cb1f65e7dc95a8c092c51528056fa481a71c7e
waleed Taib <walidtaib44@gmail.com>
2022-06-15 18:56:27 +0100

    basic authentification

--- app.js
+++ app.js
@@ -32,6 +32,33 @@ const promoRouter = require('./routes/promoRouter');
 const LeadersRouter = require('./routes/leadersRouter');
 var app = express();
 
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
