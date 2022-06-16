a3b3a73ca31da1c47227aca3146c3975f398b8a5
waleed Taib <walidtaib44@gmail.com>
2022-06-16 15:30:11 +0100

    registration succeed

--- app.js
+++ app.js
@@ -47,44 +47,28 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
     console.log(req.session);
 
-    if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');                        
-            err.status = 401;
-            next(err);
-            return;
-        }
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            req.session.user = 'admin';
-            next(); // authorized
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
     }
     else {
-        if (req.session.user === 'admin') {
-            console.log('req.session: ',req.session);
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
-        }
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
     }
+  }
 }
-
 
 app.use(auth);
 
@@ -98,8 +82,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',LeadersRouter);
