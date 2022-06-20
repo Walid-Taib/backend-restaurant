f0d433486694d5c61b1ec4ccaecfcacfe4589664
waleed Taib <walidtaib44@gmail.com>
2022-06-20 23:35:31 +0100

    https

--- app.js
+++ app.js
@@ -36,7 +36,14 @@ const LeadersRouter = require('./routes/leadersRouter');
 var app = express();
 
 
-
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
