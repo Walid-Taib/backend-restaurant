f9cbbcc7d0539df343cbb5356beac385225f2b11
waleed Taib <walidtaib44@gmail.com>
2022-06-22 17:15:46 +0100

    add favoite Id

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -40,19 +40,15 @@ FavoriteRouter.route('/')
                                 res.statusCode=200;
                                 res.setHeader('Content-Type','application/json')
                                 res.json(favorite)
-                            })
-                            
-                            
+                            })   
                         })
                     })
-
                 }
                 else{
                     err=new Error('the dish is already exist')
                     err.statusCode=404;
                     next(err);
                 }
-
            })
         }
         else{
@@ -70,15 +66,9 @@ FavoriteRouter.route('/')
                         res.setHeader('Content-Type','application/json')
                         res.json(favorite)
                     })
-                    
-                    
                 })            })
         }
-
     })
-
-
-
 })
 .delete((req,res,next)=>{
     Favorites.remove({})
@@ -88,7 +78,77 @@ FavoriteRouter.route('/')
         res.json(favo)
     })
 })
+.put((req,res,next)=>{
+    res.statusCode=404;
+    res.setHeader('Content-Type','applicaton/json');
+    res.end('Operation put is not available')
+})
 
+FavoriteRouter.route('/:DishId')
+.get((req,res,next)=>{
+    Favorites.findOne({author:req.user._id})
+    .then((resp)=>{
+        if(resp){
+            Favorites.findById(req.params.DishId)
+            .populate('author')
+            .populate('dishes')    
+            .then((favorite) => {
+                if (favorite != null ) {
+        
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(favorite);
+                }
+                else if (dish == null) {
+                    err = new Error('favorite ' + req.params.DishId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('favprote ' + req.params.DishId + ' not found');
+                    err.status = 404;
+                    return next(err);            
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+        }
+        else{
+            err = new Error('user not fine');
+            err.status = 404;
+            return next(err);              
+        }
+    })
+})
+.post((req,res,next)=>{
+    res.statusCode=404;
+    res.setHeader('Content-Type','application/json');
+    res.end('Operation is not available')
+})
+.delete((req,res,next)=>{
+    Favorites.findOne({author:req.user._id})
+    .then((resp)=>{
+        if(resp){
+            Favorites.findByIdAndRemove({_id:req.params.DishId})
+            .then((resp)=>{
+                resp.save()
+                .then((resp)=>{
+                    Favorites.findOne({author:req.user._id})
+                    .populate('author')
+                    .populate('dishes')
+                    .then((favo)=>{
+                        res.statusCode=200
+                        res.setHeader('Content-Type','application/json')
+                        res.json(favo)
+                    })
+                })
+            })
+        }
+        else{
+            res.statusCode=200;
+            res.setHeader('Content-Type','application/json')
+            console.log("error") }
+    })
+})
 
 
 module.exports=FavoriteRouter;
